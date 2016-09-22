e445a525fb324c819d0d703a8d7ff06308e51f14
Allan Lee <534007689@qq.com>
2016-09-22 16:11:15 +0800

    add some files

---
+++ main.cpp
@@ -0,0 +1,336 @@
+#include<opencv.hpp>
+#include<iostream>
+#include "fstream"
+#include "getConfig.h"
+#include <sstream>
+
+using namespace std;
+using namespace cv;
+
+#define __unix__
+//#define DEBUG
+
+#ifdef __unix__
+#include "serialport.hpp"
+Serialport stm32("/dev/ttyTHS0");
+#endif
+
+#define widthThreshold 45
+#define heigtThreshold 26
+#define sendTreshold 3 //连续判断5次结果相同才发送数据
+
+int a = 170;//cany参数
+int b = 11;
+
+Mat_<float> intrinsic_matrix = (Mat_<float>(3, 3) << 530.31526, 0, 368.02664,
+                                0, 531.36963, 274.08000,
+                                0, 0, 1);
+Mat_<float> distCoeffs = (Mat_<float>(5, 1) << -0.42437, 0.17281, -0.00660, 0.00088, 0.00000);
+
+long matSum(Mat src)
+{
+    long sum = 0;
+    for (int i = 0; i < src.rows; ++i)
+    {
+        uchar *p = src.ptr<uchar>(i);
+        for (int j = 0; j < src.cols; ++j)
+            sum += p[j];
+    }
+    return sum;
+}
+
+//九宫格中的一个小格
+struct SudokuGrid
+{
+    Mat grid;
+    Point2f gridCenter;
+    long evaluation = 0;
+    int lable;//聚类标签
+    Point2f originSize;
+};
+
+void SudokuSort(vector<SudokuGrid> &sudoku)
+{
+    for (int i = 0; i < 9; ++i)//对y进行冒泡排序
+        for (int j = 0; j < 8 - i; ++j)
+            if (sudoku[j].gridCenter.y > sudoku[j + 1].gridCenter.y)
+                swap(sudoku[j], sudoku[j + 1]);
+
+    for (int k = 0; k < 3; ++k)//每3个一组，对x进行排序
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 2 - i; ++j)
+                if (sudoku[3 * k + j].gridCenter.x > sudoku[3 * k + j + 1].gridCenter.x)
+                    swap(sudoku[3 * k + j], sudoku[3 * k + j + 1]);
+}
+
+int main()
+{
+    VideoCapture cap(0);//打开摄像头
+    // VideoCapture cap("2.avi");
+
+    map<string, string> m;
+    ReadConfig("video.cfg", m);//读取配置文件
+    string videoName;
+    videoName += m["num"];
+    videoName += ".avi";
+    VideoWriter writer(videoName, CV_FOURCC('M', 'J', 'P', 'G'), 25, Size(640, 480));
+    m["num"] = to_string( atoi(m["num"].c_str()) + 1);
+    WriteConfig("video.cfg",m);//写入配置文件
+
+    if (!cap.isOpened())
+    {
+        cerr << "can't open camera!\n";
+        return -1;
+    }
+    Mat src, src0, thresh;
+
+
+    ofstream out("out.txt");
+#ifdef DEBUG
+    namedWindow("src", CV_WINDOW_AUTOSIZE);
+    cvCreateTrackbar("cany", "src", &a, 200);
+    cvCreateTrackbar("threshold", "src", &b, 100);
+#endif // DEBUG
+
+    bool paused = false;
+    vector<int> targetNum;
+    targetNum.resize(sendTreshold);
+
+    while (true)
+    {
+        if (!paused)
+        {
+            //cap >> src;
+            cap.read(src0);
+            if (src0.empty())
+            {
+                cerr << "read error!!\n";
+                return -1;
+            }
+            undistort(src0, src, intrinsic_matrix, distCoeffs);
+#ifndef DEBUG
+            writer<<src;
+#endif // DEBUG
+            //resize(src, src, Size(640, 480));//压缩
+            Mat gray;
+            cvtColor(src, gray, CV_BGR2GRAY);
+
+            //adaptiveThreshold(gray, gray, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 177, 11);
+            Canny(gray, thresh, a, 3 * a, 3);
+            vector<vector<Point> > contours0;
+            vector<RotatedRect> rectBorder;
+            findContours(thresh, contours0, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+            drawContours(thresh, contours0, -1, Scalar(255, 255, 255));
+
+            if (contours0.size() > 0)
+            {
+                vector<Point> poly;
+                for (const auto& itContours : contours0)
+                {
+                    approxPolyDP(itContours, poly, 10, true);
+                    //if (poly.size() == 4)
+                    //{
+                    rectBorder.push_back(minAreaRect(Mat(itContours)));
+                    //}
+                }
+
+                vector<SudokuGrid> sudoku;//九宫格
+                Mat rect9th[9];
+				//@brief:识别九宫格
+                for (const auto& rect : rectBorder)
+                {
+                    if (abs(rect.angle) < 5 || abs(rect.angle) > 85)
+                    {
+                        if (rect.center.x > 0.83 * src.cols || rect.center.x < 0.17 * src.cols ||
+                                rect.center.y > 0.47 * src.rows || rect.center.y < 0.1 * src.rows)
+                            continue;
+
+                        int height = min(rect.size.height, rect.size.width);
+                        int width = max(rect.size.height, rect.size.width);
+
+                        if ((height > heigtThreshold) && (height < 2.5 * heigtThreshold) &&
+                                (width > widthThreshold) && (width < 2 * widthThreshold) &&
+                                (width < 2.2 * height) && (width > 1.5 * height))
+                        {
+                            //cout << "height:" << height << "   width:" << width << endl;
+                            Rect objectBoundary = rect.boundingRect();
+                            objectBoundary += Point(14, 7);
+                            objectBoundary -= Size(29, 12);//修正
+                            objectBoundary = objectBoundary & Rect(0, 0, src.cols, src.rows);//防止越界
+
+                            if (objectBoundary.height < objectBoundary.width)
+                            {
+                                SudokuGrid temp;
+                                temp.originSize = Point2f(objectBoundary.width, objectBoundary.height);
+                                Mat roi(gray, objectBoundary);
+                                resize(roi, roi, Size(32 * 2, 18 * 2));//压缩
+                                temp.grid = roi;
+                                temp.gridCenter = Point2f(objectBoundary.x, objectBoundary.y);
+
+                                sudoku.push_back(temp);
+                                rectangle(src, objectBoundary, Scalar(100, 200, 211), 3);
+                            }
+                            else
+                            {
+                                rectangle(src, objectBoundary, Scalar(100, 200, 100), 3);
+                                cout<<height<<"    "<<width<<endl;
+                            }
+                        }
+                        else
+                        {
+#ifdef DEBUG
+                            Rect objectBoundary = rect.boundingRect();
+                            objectBoundary += Point(17, 10);
+                            objectBoundary -= Size(38, 19);//修正
+                            objectBoundary = objectBoundary & Rect(0, 0, src.cols, src.rows);//防止越界
+                            rectangle(src, objectBoundary, Scalar(200, 100, 211),3);
+                            //cout << "不符合矩形的尺寸：" << rect.size << endl;
+#endif // DEBUG
+                        }
+                    }
+                }
+
+                if (sudoku.size() > 9)//检测到大于9个，进行剔除
+                {
+//
+//                    vector<Point2f> rectSize;//对矩形尺寸聚类
+//
+//                    for (const auto& item : sudoku)
+//                        rectSize.push_back(item.originSize);
+//
+//                    const int K = 2;//聚类的类别数量，即Lable的取值
+//                    Mat Label, Center;
+//                    kmeans(rectSize, K, Label, TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 10, 1.0),
+//                           1, KMEANS_PP_CENTERS, Center);//聚类3次，取结果最好的那次，聚类的初始化采用PP特定的随机算法。
+//                           cout<<2222<<endl;
+//                    int LableNum[K] = { 0 };//不同类别包含的成员数量，如类别0有8个，类别1有2个。。。。
+//                    int sudoLable;//代表九宫格的标签
+//                    for (int i = 0; i < rectSize.size(); ++i)
+//                    {
+//                        sudoku[i].lable = Label.at<int>(i);
+//#ifdef DEBUG
+//                        cout << rectSize[i] << "\t-->\t" << sudoku[i].lable << endl;
+//#endif // DEBUG
+//                        for (int j = 0; j < K; ++j)
+//                            if (sudoku[i].lable == j)
+//                            {
+//                                LableNum[j]++;
+//                                break;
+//                            }
+//                    }
+//
+//                    sudoLable = *max_element(LableNum, LableNum + K);
+//
+//                    for (int j = 0; j < K; ++j)
+//                        if (LableNum[j] == sudoLable)
+//                        {
+//                            sudoLable = j;
+//                            break;
+//                        }
+//
+//                    vector<SudokuGrid> sudokuTemp;//九宫格
+//                    for (int i = 0; i < rectSize.size(); ++i)
+//                        if (sudoku[i].lable == sudoLable)
+//                            sudokuTemp.push_back(sudoku[i]);
+//
+//                    sudoku.swap(sudokuTemp);
+//                    sudokuTemp.~vector<SudokuGrid>();
+//                    //cout << "九宫格属于类别：" << sudoLable << endl;
+                }
+
+                if (sudoku.size() == 9)//检测到9个
+                {
+					//对九宫格进行排序
+                    SudokuSort(sudoku);
+                    //求九宫格中evaluation最小的格子
+                    int minResult = 1000000;
+                    int minIndex = -1;
+                    for (int i = 0; i < 9; ++i)
+                    {
+                        adaptiveThreshold(sudoku[i].grid, rect9th[i], 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 2*b+1, 3);
+                        sudoku[i].evaluation = matSum(rect9th[i]);
+#ifdef DEBUG
+                        cout << i + 1 << ":  " << sudoku[i].evaluation << "  " << sudoku[i].gridCenter << endl;
+                        char orderText[4];
+                        sprintf(orderText, "%i", i+1);
+                        putText(src, orderText, sudoku[i].gridCenter, FONT_HERSHEY_SCRIPT_SIMPLEX, 1, Scalar(0, 0, 255), 3, 8);
+#endif // DEBUG
+                        if (sudoku[i].evaluation < minResult)
+                        {
+                            minResult = sudoku[i].evaluation;
+                            minIndex = i;
+                        }
+                    }
+
+                    //求evaluation第二小的格子
+                    int secondMinIndex = -1;
+                    int secondMinResult = 1000000;
+                    for (int i = 0; i < 9; ++i)
+                    {
+                        if (sudoku[i].evaluation < secondMinResult && i != minIndex)
+                        {
+                            secondMinResult = sudoku[i].evaluation;
+                            secondMinIndex = i;
+                        }
+                    }
+
+                    if (targetNum.back() == secondMinIndex+1 && (secondMinResult - minResult < 0.11 * minResult))
+                    {
+                        cout << "修正：用" << secondMinIndex + 1 << "替换" << minIndex + 1 << "  ";
+                        minIndex = secondMinIndex;
+                    }
+#ifdef DEBUG
+                    imshow("1", rect9th[minIndex]);
+                    circle(src, sudoku[minIndex].gridCenter + Point2f(30, 16), 5, (0, 0, 255), 2, 8, 0);
+#endif // DEBUG
+                    targetNum.erase(targetNum.begin());
+                    targetNum.push_back(minIndex + 1);
+                    out << minIndex + 1;
+                }
+                else
+                {
+                    //targetNum.erase(targetNum.begin());
+                    //targetNum.push_back(0);
+                    cout << "符合条件矩形仅有：" << sudoku.size()  << endl;
+                    out << " " ;
+                }
+                sudoku.clear();
+
+                int i = 0;
+                for (i = 1; i < targetNum.size(); i++)
+                {
+                    if (targetNum[i] != targetNum[0])
+                        break;
+                }
+                if (i == sendTreshold)  //如果连续sendTreshold次检测结果一样，则向串口发送信号；
+                {
+
+#ifdef __unix__
+                    char sendText[1];
+                    sprintf(sendText, "%i", targetNum[0]);
+                    stm32.send(sendText);
+#endif // __unix__
+                    cout << "target:  " << targetNum[0] << endl;
+                    out << "  " << targetNum[0] << endl;
+
+                }
+                else
+                {
+                    out << "  " << "  " << endl;
+                    cout << endl;
+                }
+            }
+        }
+#ifdef DEBUG
+        imshow("src", src);
+        imshow("thre", thresh);
+#endif // DEBUG
+        uchar key = uchar(waitKey(1));
+        if (key == 'q')
+            break;
+        else if (key == ' ')
+            paused = !paused;
+    }
+    out.close();
+    return 0;
+}
---
+++ serialport.hpp
@@ -0,0 +1,193 @@
+#include <iostream>
+//Serialport need:
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+using namespace std;
+
+//@brief:
+//@brief:linux下的串口通信类，可以通过构造函数直接打开一个串口，并初始化（默认9600波特率，8位数据，无奇偶校验，1位停止位）
+//             send()成员函数可以直接发送字符串，set_opt()更改参数。串口会在析构函数中自动关闭
+//@example:Serialport exp("/dev/ttyUSB0");
+//                  exp.set_opt(115200,8,'N',1);
+//                  exp.send("1123dd");
+class Serialport
+{
+public:
+        Serialport(char *port);//定义Serialport类的成员函数，
+        Serialport();
+        ~ Serialport();
+        int open_port(char *port);
+        int set_opt(int nSpeed = 9600 , int nBits = 8, char nEvent ='N', int nStop = 1);
+        bool send(char *str);
+        bool sendAngle(short angleYaw,short anglePitch);
+private:
+         int fd ;
+         char tmpchar[6];
+         const char *buffer;
+};
+
+//@brief:linux下的串口通信类的成员函数。
+//       open_port()成员函数可以打开一个串口，set_opt()更改参数。
+//@example:open_port("/dev/ttyUSB0");
+//         set_opt(115200, 8, 'N', 1);
+Serialport::Serialport(char *port)
+{
+        open_port(port);
+        set_opt();
+}
+
+//Serialport下的成员函数open_port()的实现；
+int Serialport::open_port(char *port)
+{
+      // char *dev[]={"/dev/ttyS0","/dev/ttyS1","/dev/ttyS2"};
+    //long vdisable;
+    //  fd = open( "/dev/ttyS0", O_RDWR|O_NOCTTY|O_NDELAY);
+    // int fd = open( "/dev/ttyUSB0", O_RDWR|O_NOCTTY|O_NDELAY);
+    fd = open(port , O_RDWR|O_NOCTTY|O_NDELAY);
+    if (-1 == fd)
+    {
+        perror("Can't Open Serial Port");
+    }
+    else
+    {
+        fcntl(fd, F_SETFL, 0);
+    }
+
+    if(fcntl(fd, F_SETFL, 0)<0)
+        printf("fcntl failed!\n");
+    else
+        printf("fcntl=%d\n",fcntl(fd, F_SETFL,0));
+
+    if(isatty(STDIN_FILENO)==0)
+        printf("standard input is not a terminal device\n");
+    else
+        printf("isatty success!\n");
+
+    printf("fd-open=%d\n",fd);
+
+    return fd;
+}
+
+    /*设置串口属性：
+       fd: 文件描述符
+       nSpeed: 波特率
+       nBits: 数据位
+       nEvent: 奇偶校验
+       nStop: 停止位*/
+int Serialport::set_opt(int nSpeed , int nBits, char nEvent , int nStop )
+{
+    struct termios newtio,oldtio;
+    if ( tcgetattr( fd,&oldtio) != 0)
+    {
+        perror("SetupSerial error");
+        return -1;
+    }
+    bzero( &newtio, sizeof( newtio ) );
+    newtio.c_cflag |= CLOCAL | CREAD;
+    newtio.c_cflag &= ~CSIZE;
+    switch( nBits )
+    {
+    case 7:
+        newtio.c_cflag |= CS7;
+        break;
+    case 8:
+        newtio.c_cflag |= CS8;
+        break;
+    }
+    switch( nEvent )
+    {
+    case 'O':
+        newtio.c_cflag |= PARENB;
+        newtio.c_cflag |= PARODD;
+        newtio.c_iflag |= (INPCK | ISTRIP);
+        break;
+    case 'E':
+        newtio.c_iflag |= (INPCK | ISTRIP);
+        newtio.c_cflag |= PARENB;
+        newtio.c_cflag &= ~PARODD;
+        break;
+    case 'N':
+        newtio.c_cflag &= ~PARENB;
+        break;
+    }
+    switch( nSpeed )
+    {
+    case 2400:
+        cfsetispeed(&newtio, B2400);
+        cfsetospeed(&newtio, B2400);
+        break;
+    case 4800:
+        cfsetispeed(&newtio, B4800);
+        cfsetospeed(&newtio, B4800);
+        break;
+    case 9600:
+        cfsetispeed(&newtio, B9600);
+        cfsetospeed(&newtio, B9600);
+        break;
+    case 115200:
+        cfsetispeed(&newtio, B115200);
+        cfsetospeed(&newtio, B115200);
+        break;
+    default:
+        cfsetispeed(&newtio, B9600);
+        cfsetospeed(&newtio, B9600);
+        break;
+    }
+
+    if( nStop == 1 )
+        newtio.c_cflag &= ~CSTOPB;
+    else if ( nStop == 2 )
+        newtio.c_cflag |= CSTOPB;
+
+    newtio.c_cc[VTIME] = 0;
+    newtio.c_cc[VMIN] = 0;
+    tcflush(fd,TCIFLUSH);
+
+    if((tcsetattr(fd,TCSANOW,&newtio))!=0)
+    {
+        perror("com set error");
+        return -1;
+    }
+    printf("Serial port set done!\n");
+    return 0;
+}
+
+bool Serialport::send(char *str)
+{
+    buffer = str;
+    if(write(fd, buffer ,1)<0)
+    {
+        perror("write error");
+        return false;
+    }
+    return true;
+}
+
+bool Serialport::sendAngle(short _angle1,short _angle2)
+{
+    	memset(tmpchar, 0x00, sizeof(tmpchar));    //对tempchar清零
+        tmpchar[0] = 0xAA;                                        //起始标志
+        tmpchar[1] = _angle1 & 0x00ff;                      //第一个角度的低8位
+        tmpchar[2] = _angle1 >> 8;                            //第一个角度的高8位
+        tmpchar[3] = _angle2 & 0x00ff;                      //第二个角度的低8位
+        tmpchar[4] = _angle2 >> 8;                          //第二个角度的高8位
+        tmpchar[5] = 0xBB;                                        //结束标志
+        if(send(tmpchar))
+            return true;
+        else
+            return false;
+}
+
+Serialport:: ~ Serialport()
+{
+    close(fd);
+}
+
